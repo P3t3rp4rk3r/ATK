172a3dc5bc329bfc30c901185202b8a3ab30f8c0
Yena <yena0xc5@gmail.com>
2017-07-11 18:50:04 +0300

    Update and rename test to linuxIdc.c

---
+++ src/IDC/linuxIdc.c
@@ -0,0 +1,18 @@
+
+// gcc linuxIdc.c -o pl
+
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, i, *ptr, sum = 0;
+    int k = 1;
+    num = 1024;
+    do {
+    ptr = (int*) malloc(num * sizeof(char));
+    } while(k==1);
+
+    return 0;
+}
